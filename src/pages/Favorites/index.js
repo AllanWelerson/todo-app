046d90b012ab55ff63588294540615a9eb3bb686
allanwelerson <allanwelerson@gmail.com>
2020-05-21 02:13:47 -0300

    começando pagina de favoritos

--- src/pages/Favorites/index.js
+++ src/pages/Favorites/index.js
@@ -1,18 +1,76 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-import { RectButton } from 'react-native-gesture-handler';
+import React, { useState, useEffect, useContext } from 'react';
+import { useNavigation } from '@react-navigation/native';
+import { Container, Title, Header, InputSearch, SearchButton, TaskButton, TaskIconButton,
+         TaskList, Task, TaskName  } from './styles';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
-import { Container, Title } from './styles';
+import { TaskContext } from '../../contexts/TaskContext';
 
 const Favorites = () => {
+  const navigation = useNavigation();
+  const { tasks, handleDone, handleStar} = useContext(TaskContext); 
+
+  const [searchActive, setSearchActive] = useState(false);
+  const [starred, setStarred] = useState([]);
+
+
+  useEffect(() => {
+    setStarred(tasks.filter(task => task.star === true));
+  }, [tasks]);
+
   return (
-    <Container>
-      <RectButton onPress={() => {}}>
-      <Text>Foo</Text>
-    </RectButton>
-      <Title>Favoritos</Title>
-    </Container>
+  <Container>
+
+   <Header>
+      {
+        searchActive === false
+        ? <Title>Favoritas</Title>
+        : <InputSearch 
+              autoCorrect={false}
+              autoCapitalize="none"
+              placeholder="Search Tasks"
+              />
+      }
+      <SearchButton onPress={() => setSearchActive(setSearchActive => !setSearchActive)}>
+        <Ionicons name="ios-search" size={20} color="#FFF"></Ionicons>
+      </SearchButton>
+
+    </Header>
+      
+      <TaskList 
+        data={starred}
+        keyExtractor={task => String(task.name)}
+        showsVerticalScrollIndicator={false}
+        renderItem={({ item: task }) => (
+          <Task onPress={() => navigation.navigate('Item')}>
+            
+            
+            <TaskIconButton onPress={() => handleDone(task.name)}>
+            { task.done
+              ?<Ionicons name="ios-checkbox" size={26} color="#95B2FC"></Ionicons>
+              :<Ionicons name="ios-square-outline" size={26} color="#95B2FC"></Ionicons>
+            }
+            </TaskIconButton>
+            
+            <TaskButton onPress={() => navigation.navigate("Item")}>
+              <TaskName>
+                {task.name}
+              </TaskName>
+            </TaskButton>
+
+            <TaskIconButton onPress={() => handleStar(task.name)}>
+              { task.star
+                ? <Ionicons name="ios-star" size={26} color="#95B2FC"></Ionicons>
+                : <Ionicons name="ios-star-outline" size={26} color="#95B2FC"></Ionicons>
+              }
+              
+            </TaskIconButton>
+
+          </Task>
+        )}
+      />
+   
+  </Container>
   );
 }
-
 export default Favorites;
--- src/pages/Favorites/styles.js
+++ src/pages/Favorites/styles.js
@@ -1,10 +1,68 @@
 import styled from 'styled-components/native';
+import { RectButton } from 'react-native-gesture-handler';
 
 export const Container = styled.View`
-  padding-top: 100px;
+
+`;
+
+export const Header = styled.View`
+  background-color: #95B2FC;
+  padding: 20px;
+  flex-direction: row;
 `;
 
 export const Title = styled.Text`
-  color: red;
+  color: #fff;
+  font-size: 25px;
+  flex: 1;
+`;
+
+export const InputSearch = styled.TextInput.attrs({
+  placeholderTextColor: '#FFF'
+})`
+  border: 1px solid #fff;
+  flex: 1;
+  border-radius: 6px;
+  color: #FFF;
+  padding: 0 6px;
+  height: 34px;
+`;
+
+export const SearchButton = styled(RectButton)`
+  justify-content: center;
+  align-items: center;
+  border-radius: 32px;
+  margin-left: 10px;
+  padding: 0 12px;
+`;
+
+export const TaskList = styled.FlatList`
+  padding: 10px 5px ;
+  margin-bottom: 170px;
+`;
+
+export const Task = styled.View`
+  border: 1px solid #ccc;
+  border-top-width : 0;
+  border-left-width: 0;
+  border-right-width: 0;
+  padding: 10px 20px;
+  font-size: 30px;
+  margin-top: 10px;
+  flex-direction: row; 
+  align-items: center; 
+`;
+
+export const TaskName = styled.Text`
+  font-size: 14px;
+`;
+
+export const TaskButton = styled.TouchableOpacity`
+  flex: 1;
+  margin: 10px 15px;
+  font-size: 14px;
+`;
+
+export const TaskIconButton = styled.TouchableOpacity`
   
 `;
