8b65b57b1a4ba79adc55525a3338de179b4a6484
allanwelerson <allanwelerson@gmail.com>
2020-06-19 01:52:22 -0300

    imagem simples no perfil

--- src/pages/Perfil/index.js
+++ src/pages/Perfil/index.js
@@ -1,12 +1,26 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { View, Image, Button } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import Constants from 'expo-constants';
 
-import { Container, Title } from './styles';
+import { Container, Photo, Name } from './styles';
+
+import user from '../../assets/user.png';
 
 const Perfil = () => {
+
+  const urlImage = require('../../assets/user.png');
+  const [image, setImage] = useState(null);
+
+  useEffect(() => {
+    setImage(urlImage);
+  },[]);
+
   return (
     <Container>
-      <Title>Este é o perfil</Title>
+
+      <Photo source={image}/>
+      <Name>Image Perfil</Name>
     </Container>
   );
 }
--- src/pages/Perfil/styles.js
+++ src/pages/Perfil/styles.js
@@ -3,15 +3,19 @@ import Swipeable from 'react-native-gesture-handler/Swipeable';
 
 export const Container = styled.View`
   padding-top: 100px;
+  justify-content: center;
+  align-items: center;
 `;
 
-export const Title = styled.Text`
-  color: red;
-  
+export const Name = styled.Text`
+  color: #000;
+  font-size: 40px;
+  margin-top: 50px;
 `;
 
-export const Caixa = styled(Swipeable)`
-  background-color: red;
-  padding: 20px;
+export const Photo = styled.Image`
+  width: 250px;
+  height: 250px;
+  background-color: #fff;
+  border-radius: 500px;
 `;
-
